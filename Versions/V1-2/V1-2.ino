584b1ab172315be61ebefcc4417a1633e5cde360
Kudde8338 <sydney.myren@gmail.com>
2024-10-23 00:27:58 +0200

    Fixat version 3

--- Versions/V1-2/V1-2.ino
+++ Versions/V1-2/V1-2.ino
@@ -3,68 +3,132 @@
 int irSendPin = 2;
 int IR_RECEIVE_PIN = 4;
 
-int SAVE_BUTTON = 13;
-int SEND_BUTTON = 10;
+// Definiera de olika knapparna:
+// Knapp för att ändra mellan läge receive och send
+int MODE_BUTTON = 13;
 
-int address = 0x3000;
-int command = 0x4F;
-String protocol = "NEC";
+// Knappar för att välja save slot
+int SAVE1_BUTTON = 10;
+int SAVE2_BUTTON = 11;
+int SAVE3_BUTTON = 12;
 
+// Initiera de tre olika kommandon som kan sparas
+// Kommando ett
+int addressOne = 0x3000;
+int commandOne = 0x4F;
+String protocolOne = "NEC";
+
+// Kommando två
+int addressTwo = 0x3000;
+int commandTwo = 0x4F;
+String protocolTwo = "NEC";
+
+// kommando tre
+int addressThree = 0x3000;
+int commandThree = 0x4F;
+String protocolThree = "NEC";
+
+String mode = "S";
+
+
+// Körs i början av programmet
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initiera en port för att skicka debugging till
 
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Startar receiver
-  IrSender.begin(irSendPin, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Startar Sändare
+  IrSender.begin(irSendPin, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Startar sändare
 
-  pinMode(SAVE_BUTTON, INPUT);
-  pinMode(SEND_BUTTON, INPUT);
+  // Initiera alla knappar som input
+  pinMode(MODE_BUTTON, INPUT); // Lägesändraren
+  pinMode(SAVE1_BUTTON, INPUT); // Save1
+  pinMode(SAVE2_BUTTON, INPUT); // Save2
+  pinMode(SAVE3_BUTTON, INPUT); // Save3
 }
 
+// Programmet som körs om och om igen
 void loop() {
-  irReceive();
-  irSend();
-
+  if ((digitalRead(MODE_BUTTON) == HIGH) && (mode == "S")) {
+    mode = "R";
+    delay(200);
+  } else if ((digitalRead(MODE_BUTTON) == HIGH) && (mode == "R")) {
+    mode = "S";
+    delay(200);
+  } else if (mode == "S") {
+    irSend();
+  } else if (mode == "R") {
+    irReceive();
+  }
 }
 
 void irReceive() {
-  if (digitalRead(SAVE_BUTTON) == HIGH) {
-    if (IrReceiver.decode()) {
-      IrReceiver.printIRResultShort(&Serial); // Skriver komplett mottagen data i en rad
-      IrReceiver.printIRSendUsage(&Serial); // Skriver ut instruktion som krävs för att skicka motsvarande signal med IRTransmitter
+  if (IrReceiver.decode()) {
+// Avkommentera för debug:
+//    debug(IrReceiver.decodedIRData);'
 
-      address = IrReceiver.decodedIRData.address;
-      command = IrReceiver.decodedIRData.command;
+    if (IrReceiver.decodedIRData.protocol == SONY || IrReceiver.decodedIRData.protocol == NEC) {
+      irSave(IrReceiver.decodedIRData);
+    }
+    IrReceiver.resume();
+  }
+}
 
-      if (IrReceiver.decodedIRData.protocol == SONY) {
-        protocol = "SONY";
-      } else if (IrReceiver.decodedIRData.protocol == NEC) {
-        protocol = "NEC";
-      }
+void irSave(IRData data) {
+  while ((digitalRead(SAVE1_BUTTON == LOW)) && (digitalRead(SAVE2_BUTTON) == LOW) && (digitalRead(SAVE3_BUTTON) == LOW)) {
+    delay(50);
+  }
+  if (digitalRead(SAVE1_BUTTON) == HIGH) {
+    addressOne = data.address;
+    commandOne = data.command;
+    protocolOne = (data.protocol == SONY) ? "SONY" : "NEC";
 
-      Serial.println("Address: ");
-      Serial.println(address, HEX);
+  } else if (digitalRead(SAVE2_BUTTON) == HIGH) {
+    addressTwo = data.address;
+    commandTwo = data.command;
+    protocolTwo = (data.protocol == SONY) ? "SONY" : "NEC";
 
-      Serial.println("");
-      Serial.println("Command: ");
-      Serial.println(command, HEX);
+  } else if (digitalRead(SAVE3_BUTTON) == HIGH) {
+    addressThree = data.address;
+    commandThree = data.command;
+    protocolThree = (data.protocol == SONY) ? "SONY" : "NEC";
+  }
 
-      Serial.println("");
-      Serial.println("Protocol: ");
-      Serial.println(protocol);
+    IrReceiver.resume();
+}
 
-      Serial.println("");
-
-      IrReceiver.resume();
+void irSend() {
+  if (digitalRead(SAVE1_BUTTON) == HIGH) {
+    if (protocolOne == "SONY") {
+      IrSender.sendSony(addressOne, commandOne, 3);
+    } else if (protocolOne == "NEC") {
+      IrSender.sendNEC(addressOne, commandOne, 3);
+    }
+  } else if (digitalRead(SAVE2_BUTTON) == HIGH) {
+    if (protocolTwo == "SONY") {
+      IrSender.sendSony(addressTwo, commandTwo, 3);
+    } else if (protocolTwo == "NEC") {
+      IrSender.sendNEC(addressTwo, commandTwo, 3);
+    }
+  } else if (digitalRead(SAVE3_BUTTON) == HIGH) {
+    if (protocolThree == "SONY") {
+      IrSender.sendSony(addressThree, commandThree, 3);
+    } else if (protocolThree == "NEC") {
+      IrSender.sendNEC(addressThree, commandThree, 3);
     }
   }
 }
 
-void irSend() {
-  if (digitalRead(SEND_BUTTON) == HIGH) {
-    if (protocol == "SONY") {
-      IrSender.sendSony(address, command, 3);
-    } else if (protocol == "NEC") {
-      IrSender.sendNEC(address, command, 3);
-    }
-  }
+void debug(IRData data) {
+
+  IrReceiver.printIRResultShort(&Serial); // Skriver komplett mottagen data i en rad
+  IrReceiver.printIRSendUsage(&Serial); // Skriver ut instruktion som krävs för att skicka motsvarande signal med IRTransmitter
+  Serial.println("Address: ");
+  Serial.println(data.address, HEX);
+  Serial.println("");
+  Serial.println("Command: ");
+  Serial.println(data.command, HEX);
+  Serial.println("");
+  Serial.println("Protocol: ");
+  Serial.println(data.protocol);
+  Serial.println("");
+
 }
